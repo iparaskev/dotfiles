99d8c24df79420523ca88dc3bba774deb81f4257
Iasonas Paraskevopoulos <iaswnparaskev@gmail.com>
2019-01-06 23:48:07 +0200

    Add download vim plugins

--- install/install_vim.sh
+++ install/install_vim.sh
@@ -9,4 +9,12 @@ then
 	mv $TARGET $BAK
 fi
 
+mkdir -p vim/bundle
+
+git clone https://github.com/VundleVim/Vundle.vim.git vim/bundle/Vundle.vim
+git clone git://github.com/jiangmiao/auto-pairs.git vim/bundle/auto-pairs
+git clone https://github.com/scrooloose/nerdtree.git vim/bundle/nerdtree
+git clone https://github.com/vim-syntastic/syntastic vim/bundle/syntastic
+git clone https://github.com/ap/vim-buftabline vim/bundle/vim-buftabline
+
 ln -s `pwd`/vim $TARGET
