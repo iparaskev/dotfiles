279df08bd82cbc29fc88ff605e560fb4cd8626d4
Iasonas Paraskevopoulos <iaswnparaskev@gmail.com>
2019-03-16 16:23:41 +0200

    Change base16 color to gruvbox

--- base16-shell/base16-current.sh
+++ base16-shell/base16-current.sh
@@ -1,32 +1,32 @@
 #!/bin/sh
 # base16-shell (https://github.com/chriskempson/base16-shell)
 # Base16 Shell template by Chris Kempson (http://chriskempson.com)
-# Darktooth scheme by Jason Milkins (https://github.com/jasonm23)
+# Gruvbox dark, hard scheme by Dawid Kurek (dawikur@gmail.com), morhetz (https://github.com/morhetz/gruvbox)
 
-color00="1D/20/21" # Base 00 - Black
-color01="FB/54/3F" # Base 08 - Red
-color02="95/C0/85" # Base 0B - Green
-color03="FA/C0/3B" # Base 0A - Yellow
-color04="0D/66/78" # Base 0D - Blue
-color05="8F/46/73" # Base 0E - Magenta
-color06="8B/A5/9B" # Base 0C - Cyan
-color07="A8/99/84" # Base 05 - White
-color08="66/5C/54" # Base 03 - Bright Black
+color00="1d/20/21" # Base 00 - Black
+color01="fb/49/34" # Base 08 - Red
+color02="b8/bb/26" # Base 0B - Green
+color03="fa/bd/2f" # Base 0A - Yellow
+color04="83/a5/98" # Base 0D - Blue
+color05="d3/86/9b" # Base 0E - Magenta
+color06="8e/c0/7c" # Base 0C - Cyan
+color07="d5/c4/a1" # Base 05 - White
+color08="66/5c/54" # Base 03 - Bright Black
 color09=$color01 # Base 08 - Bright Red
 color10=$color02 # Base 0B - Bright Green
 color11=$color03 # Base 0A - Bright Yellow
 color12=$color04 # Base 0D - Bright Blue
 color13=$color05 # Base 0E - Bright Magenta
 color14=$color06 # Base 0C - Bright Cyan
-color15="FD/F4/C1" # Base 07 - Bright White
-color16="FE/86/25" # Base 09
-color17="A8/73/22" # Base 0F
-color18="32/30/2F" # Base 01
+color15="fb/f1/c7" # Base 07 - Bright White
+color16="fe/80/19" # Base 09
+color17="d6/5d/0e" # Base 0F
+color18="3c/38/36" # Base 01
 color19="50/49/45" # Base 02
-color20="92/83/74" # Base 04
-color21="D5/C4/A1" # Base 06
-color_foreground="A8/99/84" # Base 05
-color_background="1D/20/21" # Base 00
+color20="bd/ae/93" # Base 04
+color21="eb/db/b2" # Base 06
+color_foreground="d5/c4/a1" # Base 05
+color_background="1d/20/21" # Base 00
 
 if [ -n "$TMUX" ]; then
   # Tell tmux to pass the escape sequences through
@@ -78,13 +78,13 @@ put_template 21 $color21
 # foreground / background / cursor color
 if [ -n "$ITERM_SESSION_ID" ]; then
   # iTerm2 proprietary escape codes
-  put_template_custom Pg A89984 # foreground
-  put_template_custom Ph 1D2021 # background
-  put_template_custom Pi A89984 # bold color
+  put_template_custom Pg d5c4a1 # foreground
+  put_template_custom Ph 1d2021 # background
+  put_template_custom Pi d5c4a1 # bold color
   put_template_custom Pj 504945 # selection color
-  put_template_custom Pk A89984 # selected text color
-  put_template_custom Pl A89984 # cursor
-  put_template_custom Pm 1D2021 # cursor text
+  put_template_custom Pk d5c4a1 # selected text color
+  put_template_custom Pl d5c4a1 # cursor
+  put_template_custom Pm 1d2021 # cursor text
 else
   put_template_var 10 $color_foreground
   if [ "$BASE16_SHELL_SET_BACKGROUND" != false ]; then
